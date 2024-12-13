65603d2169977e61f0dd776a46955fb79748e05e
NNHieu <ngochieutb13@gmail.com>
2024-12-13 23:19:15 +0700

    update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Start from Random Blog',
+  author: 'Hieu Nguyen Ngoc',
+  headerTitle: 'Magenta Blog',
+  description: 'A blog',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
